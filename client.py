6587617ff646718c5108467d09412cba3433da24
anupam <godsean14.it@coep.ac.in>
2019-05-03 19:34:45 -0400

    First commit

---
+++ client.py
@@ -0,0 +1,250 @@
+from multiprocessing import Process, Manager, Lock
+import os
+import time
+import socket
+import sys
+import re
+from os import listdir
+from os.path import isfile, join
+import platform
+from time import gmtime, strftime
+from threading import Thread
+
+#HOSTNAME=socket.gethostname()
+VERSION="P2P-CI/1.0"
+HOSTOS=platform.platform()
+#MAX_RESPONSE_SIZE=5*1024*1024 #Max file size = 5MB (assumed)
+MAX_RESPONSE_SIZE=4096 #Max file size = 5MB (assumed)
+MAX_REQUEST_SIZE=4096
+SERVER_PORT=7734
+RFCS_PATH = "./RFCs/client1/"
+OK=200
+BAD_REQUEST=400
+NOT_FOUND=404
+VERSION_NOT_SUPPORTED=505
+STATUS_CODES={OK:"OK", BAD_REQUEST:"Bad Request", NOT_FOUND:"Not Found", VERSION_NOT_SUPPORTED:"P2P-CI Version Not Supported"}
+
+SERVER_IP = '10.155.18.166'
+HOST_IP = '10.155.18.166'
+HOSTNAME = '10.155.18.166'
+
+
+
+#init
+my_rfcs = list();
+
+#list of RFCs at client
+#my_rfcs = my_rfcs + [int(re.findall(r'\d+', f)[0]) for f in listdir(RFCS_PATH) if isfile(join(RFCS_PATH, f))];
+
+#lock to perform operations on list of RFCS
+lock_my_rfcs = Lock()
+
+
+#upload port for client
+upload_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+#upload_sock.bind((HOSTNAME, 0));
+upload_sock.bind((HOST_IP, 0));
+my_upload_port = upload_sock.getsockname()[1]
+
+
+#a RFC dictionary item value (dictionary item key would be rfc number)
+class Rfc:
+    def __init__(self, number, title, hostname):
+        self.number = number
+        self.title = title
+        self.hostname = hostname
+
+#a peer would have a hostname and a port to communicate with CI server
+class Peer:
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
+
+
+def serve_peers():
+    #start listening to peer requests
+    upload_sock.listen()
+
+    while True:
+        peer_sock, peer_address = upload_sock.accept()
+        request = peer_sock.recv(MAX_REQUEST_SIZE)
+        if not request:
+            continue;
+
+        request = request.decode().split('\r\n')
+        request_row_1 = request[0].split()  
+        request_type = request_row_1[0]
+        rfc_number = int(request_row_1[2])
+        version = request_row_1[-1]
+
+        response_code = OK
+
+        data = ""
+        filepath = ""
+        response=""
+
+        if(version != VERSION):
+            response_code = VERSION_NOT_SUPPORTED
+        else:
+            #check if requested rfc is present
+            lock_my_rfcs.acquire() 
+            if(rfc_number not in my_rfcs):
+                response_code = NOT_FOUND
+            lock_my_rfcs.release()
+                
+            if(response_code == NOT_FOUND):
+                response = VERSION+" "+str(response_code)+" "+STATUS_CODES[response_code]+"\r\n"+\
+                            "\r\n"
+            else:
+                filepath = RFCS_PATH+'rfc'+str(rfc_number)+'.txt'
+                current_time = strftime("%a, %d %b %Y %X GMT", gmtime())
+                modified_time = strftime("%a, %d %b %Y %X GMT", time.localtime(os.path.getmtime(filepath)))
+                with open(filepath, 'r') as myfile:
+                      data = myfile.read()
+
+                data_length = str(len(data))
+
+                response = VERSION+" "+str(response_code)+" "+STATUS_CODES[response_code]+"\r\n"+\
+                            "Date: "+current_time+"\r\n"+\
+                            "OS: "+HOSTOS+"\r\n"+\
+                            "Last-Modified: "+ modified_time+"\r\n"+\
+                            "Content-Length: " + data_length+"\r\n"+\
+                            "Content Type: text/text\r\n"+\
+                            data+"\r\n"+\
+                            "\r\n"
+
+        peer_sock.sendall(response.encode())
+
+
+
+#Add available RFC to the server
+def add_rfc(sock, rfc):
+    rfc_title = rfc.split('.')[0]
+    rfc_number = int(rfc_title[3:])
+    add_request = "ADD"+" "+"RFC "+str(rfc_number)+" "+VERSION+"\r\n"+\
+                    "Host:"+" "+HOSTNAME+"_"+str(my_upload_port)+"\r\n"+\
+                    "Port:"+" "+str(my_upload_port)+"\r\n"+\
+                    "Title:"+" "+rfc_title+"\r\n"+\
+                    "\r\n";
+    
+    #print(add_request)
+
+    sock.sendall(add_request.encode());
+
+    response = sock.recv(MAX_RESPONSE_SIZE).decode();
+    return response;
+
+#lookup  RFC
+def lookup_rfc(sock, rfc_number, my_upload_port, rfc_title):
+    lookup_request = "LOOKUP"+" "+"RFC "+str(rfc_number)+" "+VERSION+"\r\n"+\
+                    "Host:"+" "+HOSTNAME+"_"+str(my_upload_port)+"\r\n"+\
+                    "Port:"+" "+str(my_upload_port)+"\r\n"+\
+                    "Title:"+" "+rfc_title+"\r\n"+\
+                    "\r\n";
+    
+    #print(lookup_request)
+
+    sock.sendall(lookup_request.encode());
+
+    response = sock.recv(MAX_RESPONSE_SIZE).decode();
+    return response;
+
+#list RFCs at server
+def list_rfcs(sock, my_upload_port):
+    list_request = "LIST"+" "+"ALL "+VERSION+"\r\n"+\
+                    "Host:"+" "+HOSTNAME+"_"+str(my_upload_port)+"\r\n"+\
+                    "Port:"+" "+str(my_upload_port)+"\r\n"+\
+                    "\r\n";
+    
+    #print(list_request)
+
+    sock.sendall(list_request.encode());
+
+    response = sock.recv(MAX_RESPONSE_SIZE).decode();
+    return response;
+    
+
+def rfc_download_request(rfc_number, hostname, port):
+    download_request = "GET"+" RFC "+str(rfc_number)+" "+VERSION+"\r\n"+\
+                "Host: "+hostname+"\r\n"+\
+                "OS: "+HOSTOS+"\r\n"+\
+                "\r\n"
+
+    
+    download_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    #host_ip = socket.gethostbyname(hostname.split('_')[0])
+    #print(host_ip, type(host_ip))
+    host_ip = hostname.split('_')[0]
+    download_sock.connect((host_ip, port))
+    
+    download_sock.sendall(download_request.encode())
+    response = download_sock.recv(MAX_RESPONSE_SIZE).decode()
+    download_sock.close()
+
+    split_response = response.split('\r\n')
+    data = split_response[6]
+
+    filepath = RFCS_PATH+'rfc'+str(rfc_number)+'.txt'
+
+    with open(filepath, 'w') as myfile:
+          myfile.write(data)
+
+    return response
+
+def print_options():
+    print("1. ADD")
+    print("2. LOOKUP")
+    print("3. LIST")
+    print("4. LOGOUT")
+
+#Client main
+if __name__ == '__main__':
+    #spawn a process to serve other peers
+    p_serve_peers = Thread(target=serve_peers);
+
+    p_serve_peers.start()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    #sock.connect((HOSTNAME, SERVER_PORT))
+    sock.connect((SERVER_IP, SERVER_PORT))
+
+    #for rfc_number in my_rfcs:
+    #    #rfc_number = re.findall(r'\d+', each)[0]
+    #    rfc_title="RFC"+str(rfc_number)
+    #    response = add_rfc(sock, rfc_number, my_upload_port, rfc_title)
+    #    print("Added RFC "+ str(rfc_number) + "\nResponse from server is:\n" +response);
+
+
+    #lookup_rfc_number = 500;
+    #lookup_rfc_title = "RFC"+str(lookup_rfc_number)
+    #response = lookup_rfc(sock, lookup_rfc_number, my_upload_port, lookup_rfc_title)
+    #print("Lookup RFC "+ str(lookup_rfc_number) + "\nResponse from server is:\n" +response);
+
+    #split_response = response.split('\r\n');
+    ##print(split_response)
+
+    #rfc_details = split_response[2].split();
+    #get_rfc_from = rfc_details[2]
+    #get_rfc_from_port = int(rfc_details[3])
+
+    ##print("peer details")
+    ##print(get_rfc_from, get_rfc_from_port)
+
+    #response = rfc_download_request(lookup_rfc_number, get_rfc_from, get_rfc_from_port);
+    ##print("Download RFC\nResponse from server is:\n" +response);
+
+    #response = list_rfcs(sock, my_upload_port)
+    #print("LIST RFC\nResponse from server is:\n" +response);
+
+    while True:
+        print_options()
+        option = int(raw_input("Enter option : "))
+        if(option == 1):
+            rfc = raw_input("Enter filename : ")
+            response = add_rfc(sock, rfc)
+            print("Added RFC "+ str(rfc_number) + "\nResponse from server is:\n" +response);
+            
+
+
+
+
---
+++ server.py
@@ -0,0 +1,228 @@
+from multiprocessing import Process, Manager, Lock
+import os
+import time
+import socket
+import sys
+from threading import Thread
+
+VERSION="P2P-CI/1.0";
+MAX_BUFF_LEN=4096
+SERVER_PORT=7734
+OK=200
+BAD_REQUEST=400
+NOT_FOUND=404
+VERSION_NOT_SUPPORTED=505
+STATUS_CODES={OK:"OK", BAD_REQUEST:"Bad Request", NOT_FOUND:"Not Found", VERSION_NOT_SUPPORTED:"P2P-CI Version Not Supported"}
+#HOSTNAME=socket.gethostname()
+HOST_IP = '10.155.18.166'
+HOST_NAME = '10.155.18.166'
+
+#a list to store active peers
+peers = list();
+# a lock to synchronise active peers list operations
+lock_peers = Lock();
+
+#a dictionary to store availabe RFCs and peers containing them
+rfcs = dict();
+# a lock to synchronise rfcs dictionary operations
+lock_rfcs = Lock();
+
+#a RFC dictionary item value (dictionary item key would be rfc number)
+class Rfc:
+    def __init__(self, number, title, hostname):
+        self.number = number
+        self.title = title
+        self.hostname = hostname
+
+#a peer would have a hostname and a port to communicate with CI server
+class Peer:
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
+
+def check_peer_active(hostname, lock_peers, peers):
+    lock_peers.acquire()
+    for peer in peers:
+        if(peer.hostname == hostname):
+            lock_peers.release();
+            return True
+
+    lock_peers.release();
+    return False;
+    
+
+def send_response(connection, status_code, data):
+    response = VERSION+" "+str(status_code)+" "+STATUS_CODES[status_code]+"\r\n"+\
+                "\r\n"
+
+    for each in data:
+        response = response+each[0]+" "+each[1]+" "+each[2]+" "+each[3]+"\r\n"
+
+    response = response+"\r\n" 
+
+    connection.sendall(response.encode())
+
+
+def serve_client(connection, client_address):
+    try:
+        #serve client requests
+        while True:
+            data = connection.recv(MAX_BUFF_LEN)
+            if not data:
+                break;
+                continue;
+            #print("received "+str(data.decode()))
+            request = data.decode().split('\r\n');
+
+            request_row_1 = request[0].split();
+            request_type = request_row_1[0];
+            rfc_version = request_row_1[-1];
+
+            request_row_2=request[1].split();
+            client_hostname=request_row_2[1];
+
+            request_row_3=request[2].split();
+            client_port=int(request_row_3[1]);
+
+            response_data = [];
+            #process ADD request from client
+            if(request_type == "ADD"):
+                rfc_number = int(request_row_1[2]);
+                rfc_title = request[3].split()[1];
+                response_code=OK;
+                
+                response_data.append(["RFC"+str(rfc_number), rfc_title, client_hostname, str(client_port)])
+                #correct version check
+                if(rfc_version!=VERSION):
+                    response_code = VERSION_NOT_SUPPORTED;
+                else:
+                    #check if this peer is already present in active list
+                    is_peer_active = check_peer_active(client_hostname, lock_peers, peers)
+                    if not is_peer_active:
+                        new_peer = Peer(client_hostname, client_port)
+                        lock_peers.acquire()
+                        peers.insert(0, new_peer);
+                        lock_peers.release()
+
+                    new_rfc = Rfc(rfc_number, rfc_title, client_hostname);
+                    #Add this RFC to RFCs list
+                    lock_rfcs.acquire()
+                    #print("keys");
+                    #print(rfcs.keys())
+                    if rfc_number in rfcs.keys():
+                        #check if client has already added that RFC(avoid duplication)
+                        already_present = False;
+                        for client in rfcs[rfc_number]:
+                            if(client.hostname == client_hostname):
+                                already_present = True;
+                                #print("Already Present\n");
+                                break;
+                        if not already_present:
+                            #print("appending")
+                            #print(new_rfc.number, new_rfc.title, new_rfc.hostname)
+                            #print(type(rfcs[rfc_number]))
+                            #print(type(rfcs))
+                            rfcs[rfc_number] = rfcs[rfc_number] + [new_rfc];
+                        """for each, value in rfcs.items():
+                            print(each);
+                            for e in value:
+                                print(e.number, e.title, e.hostname)
+                            print("\n\n");"""
+                    else:
+                        #rfcs[rfc_number] = list();
+                        rfcs[rfc_number] = [new_rfc] #list();
+                        #rfcs[rfc_number].append(new_rfc);
+                    lock_rfcs.release()
+                send_response(connection, response_code, response_data);
+                """for each, value in rfcs.items():
+                    print(each);
+                    for e in value:
+                        print(e.number, e.title, e.hostname)
+                    print("\n\n");"""
+            elif(request_type=="LOOKUP"):
+                rfc_number = int(request_row_1[2]);
+                rfc_title = request[3].split()[1];
+                response_code = OK;
+
+                #correct version check
+                if(rfc_version!=VERSION):
+                    response_code = VERSION_NOT_SUPPORTED;
+                else:
+                    #check if this rfc is present
+                    lock_rfcs.acquire()
+                    lock_peers.acquire()
+                    if rfc_number in rfcs.keys():
+                        #if rfc is present add all hosts having this rfc to response_data
+                        rfcs_list = rfcs[rfc_number]
+                        for rfc in rfcs_list:
+                            for peer in peers:
+                                if(rfc.hostname == peer.hostname):
+                                    response_data.append(["RFC"+str(rfc_number), rfc_title, peer.hostname, str(peer.port)])
+                    else:
+                        response_code = NOT_FOUND;
+                    lock_peers.release()
+                    lock_rfcs.release()
+                send_response(connection, response_code, response_data);
+            elif(request_type=="LIST"):
+                """for each, value in rfcs.items():
+                    print(each);
+                    for e in value:
+                        print(e.number, e.title, e.hostname)
+                    print("\n\n");"""
+                response_code = OK;
+                lock_rfcs.acquire()
+                lock_peers.acquire()
+                for rfc_number in rfcs.keys():
+                   rfcs_list = rfcs[rfc_number] 
+                   for rfc in rfcs_list:
+                       for peer in peers:
+                           if(rfc.hostname == peer.hostname):
+                                response_data.append(["RFC"+str(rfc_number), rfc.title, peer.hostname, str(peer.port)])
+                lock_peers.release()
+                lock_rfcs.release()
+                send_response(connection, response_code, response_data);
+
+    finally:
+        # Clean up the connection
+        print("Closing connection to ", client_address)
+        connection.close()
+        exit(0)
+
+#Server main
+if __name__ == '__main__':
+    #processes
+    spawned = [];
+
+    #a well known server port to accept requests from clients
+    server_port = SERVER_PORT;
+
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+
+    # Bind the socket to the port
+    #server_address = (HOSTNAME, server_port);
+    server_address = (HOST_IP, server_port);
+    #print >>sys.stderr, 'starting up on %s port %s' % server_address
+    sock.bind(server_address);
+
+    
+    #start listening on server_port
+    sock.listen();
+     
+    i=0
+    #keep on accepting new connections from client and spawn new process for each client
+    while True:
+        print('waiting for a connection');
+        connection, client_address = sock.accept();
+
+        #p = Process(target=serve_client, args=(peers, rfcs, lock_peers, lock_rfcs, connection, client_address))
+        p = Thread(target=serve_client, args=(connection, client_address))
+        i+=1
+
+        spawned.append(p)
+
+        p.start()
+
+    #this should never be executed
+    for process in spawned:
+        process.join()
